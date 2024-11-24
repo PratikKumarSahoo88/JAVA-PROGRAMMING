85201e17d5b3c660fa7321ff4d88d4f1613196ee
Pratik Kumar Sahoo <pratiksahoo591@gmail.com>
2024-11-24 20:08:55 +0530

    JAVA PROGRAMMING

---
+++ Code1.java
@@ -0,0 +1,7 @@
+public class Code1{
+    public static void main(String[] args) {
+        String s="Java";
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+}
+} 
