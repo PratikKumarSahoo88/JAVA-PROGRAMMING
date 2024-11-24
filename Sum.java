caedacd7207c85a5b9435f678ac04a3fdb2138f7
Pratik Kumar Sahoo <pratiksahoo591@gmail.com>
2024-11-24 20:10:52 +0530

    JAVA PROGRAMMING

---
+++ Sum.java
@@ -0,0 +1,14 @@
+public class Sum {
+    public static void main(String[] args) {
+        
+        int a[]={1,20,6,89,5};
+
+        int sum=0;
+        for ( int i = 0; i < 5; i++) {
+            sum=sum+a[i];
+            
+        }
+System.out.println("sum is"+sum);            
+        }
+    }
+    
