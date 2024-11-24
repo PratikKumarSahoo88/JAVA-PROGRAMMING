53bc50323102f603ae2f7545b63d70402c472063
Pratik Kumar Sahoo <pratiksahoo591@gmail.com>
2024-11-24 20:09:52 +0530

    JAVA PROGRAMMING

---
+++ Code3.java
@@ -0,0 +1,7 @@
+public class Code3 {
+    public static void main(String[] args) {
+        String s="java programming";
+        System.out.println(s.startsWith("ja"));
+        System.out.println(s.endsWith("ng"));
+    }
+}
